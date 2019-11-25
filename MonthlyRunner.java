c6362a4ede64ace0ea5ed995f853fae6eb777300
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2019-11-25 05:01:30 +0300

    Add file reading logic

--- MonthlyRunner.java
+++ MonthlyRunner.java
@@ -17,17 +17,40 @@ public class MonthlyRunner {
 
         for (File file : allFiles) {
             try {
-                //Reads data from file
+                String fileName = file.getName();
+                // Reads data from file
+                int underscoreLocation = fileName.indexOf("_");
+                String fileType = fileName.substring(0, underscoreLocation);
+               MonthlyItem mi = createMonthlyItem(fileType);
+                // invoke a factory method that will create the appropriate class
                 Scanner scanFile = new Scanner(file);
-                //get first string out of the file
-                
+                // get first string out of the file
+
                 String data = scanFile.nextLine();
+                //Tell the monthly item object to pricess the data
+                mi.process(data);
             } catch (FileNotFoundException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
-            
+
         }
-        
+
+    }
+
+    private static MonthlyItem createMonthlyItem(String fileType) {
+        MonthlyItem monthlyItem = null;
+        if (fileType.equalsIgnoreCase(("Employee"))) {
+            monthlyItem = new Employee();
+
+        } else if (fileType.equalsIgnoreCase("Annuity")) {
+
+            monthlyItem = new Annuity();
+
+        } else if (fileType.equalsIgnoreCase("LifeInsurance")) {
+            monthlyItem = new LifeInsurance();
+
+        }
+        return monthlyItem;
     }
 }
